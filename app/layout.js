06460a37b603fdccdcae07f18f93ac0a57b2f369
masudfcs1 <masudfcs1@gmail.com>
2024-10-16 09:21:43 +0600

    clerk Auth implementation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -14,14 +15,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`inter.className`}>
-        <Header/>
-        <main className="min-h-screen bg-gradient-to-b from-blue-100 to-white ">
-          {children}
-        </main>
-      </body>
-      <Footer/>
-    </html>
+    <>
+      <ClerkProvider>
+        <html lang="en">
+          <body className={`inter.className`}>
+            <Header />
+            <main className="min-h-screen bg-gradient-to-b from-blue-100 to-white ">
+              {children}
+            </main>
+          </body>
+          {/* <Footer /> */}
+        </html>
+      </ClerkProvider>
+    </>
   );
 }
